6e376a049755e52c92c14b131d40f94b2cf02700
Александр Анисин <alexanderanisin@icloud.com>
2021-12-11 13:13:06 +0300

    Added Task 9-10

--- task9-10/task.js
+++ task9-10/task.js
@@ -174,4 +174,190 @@ function reverseArrayInPlace(array) {
 }
 console.log(reverseArrayInPlace([1, 2, 3]));
 
+console.log("--------------------------")
+
+/*
+Напишите функцию arrayToList, которая строит такую структуру, получая в качестве аргумента [1, 2, 3], 
+а также функцию listToArray, которая создаёт массив из списка. Также напишите вспомогательную функцию prepend, 
+которая получает элемент и создаёт новый список, где этот элемент добавлен спереди к первоначальному списку, и функцию nth, 
+которая в качестве аргументов принимает список и число, а возвращает элемент на заданной позиции в списке, или же undefined в случае отсутствия такого элемента.
+Если ваша версия nth не рекурсивна, тогда напишите её рекурсивную версию.
+*/
+
+function arrayToList(arr) {
+	return arr.reduceRight((rest, value) => ({value, rest}), null);
+}
+	
+console.log(arrayToList ([1, 2, 3]))
+	
+function listToArray(list) {
+	let array = [];
+	for (let node = list; node; node = node.rest) {
+	array.push(node.value);
+	}
+	return array;
+}
+
+console.log(listToArray(arrayToList([1, 2, 3])));
+
+function prepend(value, list) {
+	return {value, rest: list};
+}
+
+console.log(prepend(1, prepend(2, null)));
+
+function nth(list, n) {
+	if (!list) return undefined;
+	else if (n == 0) return list.value;
+	else return nth(list.rest, n - 1);
+}
+
+console.log(nth(arrayToList([1, 2, 3]), 1));
+
+console.log("--------------------------")
+
+/*
+Напишите функцию deepEqual, которая принимает два значения и возвращает true, только если это два одинаковых значения или это объекты, 
+свойства которых имеют одинаковые значения, если их сравнивать рекурсивным вызовом deepEqual.
+*/
+
+function deepEqual(a, b) {
+	if (a === b)
+		return true;
+	if (a == null || typeof a != "object" ||
+		b == null || typeof b != "object") 
+		return false;
+	let keysA = Object.keys(a), keysB = Object.keys(b);
+	if (keysA.length != keysB.length) 
+		return false;
+	for (let key of keysA) {
+	  	if (!keysB.includes(key) || !deepEqual(a[key], b[key])) 
+			return false;
+	}
+	return true;
+  }
+  
+let asobj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(asobj, asobj));
+
+console.log("--------------------------")
+
+/*
+Свертка. Используйте метод reduce в комбинации с concat для свёртки массива массивов в один массив, у которого есть все элементы входных массивов.
+*/
+
+function flatten(arr) {
+	return arr.reduce(function (flat, toFlatten) {
+	  	return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+	}, []);
+  }
+
+console.log(flatten([[1, 2, 3], [4, 5]]));
+
+console.log("--------------------------")
+
+/*
+Разница в возрасте матерей и их детей
+Используя набор данных из примера, подсчитайте среднюю разницу в возрасте между матерями и их детьми (это возраст матери во время появления ребёнка).
+Можно использовать функцию average, приведённую в главе.
+*/
+
+console.log("asd")
+let ancestry = ([
+	{b: 1832, d: 1905},
+	{b: 1876, d: 1956},
+	{b: 1683, d: 1724},
+	{b: 1714, d: 1748},
+	{b: 1907, d: 1997},
+	{b: 1761, d: 1833},
+	{b: 1535, d: 1582},
+	{b: 1918, d: 2012},
+	{b: 1877, d: 1968},
+	{b: 1696, d: 1724},
+	{b: 1602, d: 1642}]);
+
+
+function average(array) {
+	function plus(a, b) { return a + b; }
+	return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+	byName[person.name] = person;
+});
+
+var differences = ancestry.filter(function(person) {
+	return byName[person.mother] != null;
+}).map(function(person) {
+	return person.b - byName[person.mother].b;
+});
+console.log(average(differences));
+
+console.log("--------------------------")
+
+/*
+Историческая ожидаемая продолжительность жизни.
+Мы считали, что только последнее поколение людей дожило до 90 лет. Давайте рассмотрим этот феномен поподробнее. 
+Подсчитайте средний возраст людей для каждого из столетий. Назначаем столетию людей, беря их год смерти, 
+деля его на 100 и округляя: Math.ceil(person.died / 100).
+*/
+
+function average(array) {
+	function plus(a, b) { return a + b; }
+	return array.reduce(plus) / array.length;
+}
+
+function groupBy(array, groupOf) {
+	var groups = {};
+	array.forEach(function(element) {
+		var groupName = groupOf(element);
+		if (groupName in groups)
+		groups[groupName].push(element);
+		else
+		groups[groupName] = [element];
+	});
+	return groups;
+}
+
+var byCentury = groupBy(ancestry, function(person) {
+	return Math.ceil(person.d / 100);
+});
+
+for (var century in byCentury) {
+	var ages = byCentury[century].map(function(person) {
+		return person.d - person.b;
+	});
+	console.log(century + ": " + average(ages));
+}
+
+console.log("--------------------------")
+
+/*
+Напишите функции every и some, которые работают так же, как эти методы, только принимают массив в качестве аргумента.
+*/
+
+function every(array, predicate) {
+	for (var i = 0; i < array.length; i++) {
+		if (!predicate(array[i]))
+		return false;
+	}
+	return true;
+}
+
+function some(array, predicate) {
+	for (var i = 0; i < array.length; i++) {
+		if (predicate(array[i]))
+		return true;
+	}
+	return false;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+console.log(every([NaN, NaN, 4], isNaN));
+console.log(some([NaN, 3, 4], isNaN));
+console.log(some([2, 3, 4], isNaN));
+
+console.log("--------------------------")
+
 console.log("-конец файла-")
